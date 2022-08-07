68b1fc8c2363e4023acb3ec6a4c39da1de96dea7
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-07 23:53:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+                    const form = document.querySelector('form');
+                    const input = document.querySelectorAll('input');
+                    const li = document.querySelector('#tweetCont')
 
+                    form.addEventListener('submit',function(e){
+                        e.preventDefault();
+                        const usrname = input[0].value;
+                        const tweet = input[1].value;
+                        const newli = document.createElement('li');
+                        newli.append(usrname);
+                        newli.append(` -  ${"Tweet : "} ${tweet}`);
+                        li.append(newli);
+                        input[0].value = "";
+                        input[1].value = "";
+                    });
